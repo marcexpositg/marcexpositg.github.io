493c00166a4bd7a25d79e899dacc460a964cf152
marcexpositg <marcexgoy@gmail.com>
2019-01-21 16:08:42 -0500

    all

--- js/main.js
+++ js/main.js
@@ -12,15 +12,78 @@ Enjoy responsibly!
 
 */
 
-$(document).ready(function() {
-	
+$('#appear1').fadeIn("slow");
+setTimeout(function () {
+	$('#appear2').fadeIn("slow");
+}, 1000);
+
+function pageScroll() {
+	window.scrollBy(0, 3);
+	scrolldelay = setTimeout(pageScroll, 10);
+	scrollposition = $(window).scrollTop();
+	if ((scrollposition > 50) || (scrollposition = 0)) {
+		clearTimeout(scrolldelay);
+	};
+};
+
+function stopscrolling() {
+	clearTimeout(scrolldelay);
+}
+
+$(document).ready(function () {
+
 	//setTimeout(function(){
-		$(document).one("click", function(){
+	$(document).one("click", function () {
 		$('body').addClass('loaded');
 		//$('h1').css('color','#222222');
 		$('#loader-recommendation').fadeOut();
 		//document.getElementById('loader-recommendation').style.display = 'none';
+
+		document.getElementById('missu').play();
+
+
+		//$('.active').removeClass('active');
+		//$('#num1').parent().addClass('active');
+
+
+		//$('.carousel').carousel({
+		//	pause: true,
+		//	interval: false
+		//}).carousel(0);
+		$('.carousel').carousel(0);
+		$('.carousel').carousel('pause');
+
+		setTimeout(function () {
+			$('.carousel').carousel({
+				interval: 2000
+			})
+		}, 3000);
+
+
+		setTimeout(function () {
+			$('html, body').css({
+				'overflow': 'auto',
+				'height': 'auto'
+			});
+			$('#pleaseScroll').fadeIn("slow");
+			setTimeout(function(){pageScroll();},2000);
+		}, 5000);
+
+		//	console.log($(window).scrollTop());
+		//	if ($(window).scrollTop() > "30px") {
+		//		console.log("Que gran tio");
+		//	}
+
+		$(document).mousemove(stopscrolling);
+
+
+
+		//$('.carousel').carousel({
+		//			interval: 2000
+		//		  })
+
+
 	})
 	//}, 3000);
-	
+
 });
